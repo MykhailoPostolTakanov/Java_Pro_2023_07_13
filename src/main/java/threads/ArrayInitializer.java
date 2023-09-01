ff84125603c7050c9131a251051dc81558043ae0
Misha <mishagertar@gmail.com>
2023-09-01 14:59:36 +0300

    Update HomeWork #15 about threads.

--- src/main/java/threads/ArrayInitializer.java
+++ src/main/java/threads/ArrayInitializer.java
@@ -2,8 +2,6 @@ package threads;
 
 public class ArrayInitializer {
     private static double[] arr;
-    private static double[] array1;
-    private static double[] array2;
 
     public static double[] getArr() {
         return arr;
@@ -13,49 +11,37 @@ public class ArrayInitializer {
         if (array.length % 2 != 0)
             return;
         arr = new double[array.length];
-        initHelperArray(array);
 
         Thread firstArrayThread = new Thread(() -> {
             System.out.println("Start firstArrayThread");
-            mathArray(array1);
+            mathArray(array, true);
             System.out.println("End firstArrayThread");
         });
 
         Thread secondArrayThread = new Thread(() -> {
             System.out.println("Start secondArrayThread");
-            mathArray(array2);
+            mathArray(array, false);
             System.out.println("End secondArrayThread");
         });
 
-        firstArrayThread.start();;
+        firstArrayThread.start();
         secondArrayThread.start();
 
         firstArrayThread.join();
         secondArrayThread.join();
 
-        mergeArray();
+        mergeArray(array);
     }
 
-    private static void initHelperArray(double[] array) {
-        int length = array.length;
-        array1 = new double[(length) / 2];
-        array2 = new double[length - array1.length];
-        for (int i = 0; i < length; i++)
-            if (i < array1.length)
-                array1[i] = array[i];
-            else
-                array2[i - array1.length] = array[i];
+    private static void mathArray(double[] array, boolean isFirstPart) {
+        int indexForParts = 0;
+        if (!isFirstPart)
+            indexForParts = array.length / 2;
+        for (var i = 0; i < array.length / 2; i++)
+            array[i + indexForParts] = (array[i + indexForParts] * Math.sin(0.2 + i / 5.0) * Math.cos(0.2 + i / 5.0) * Math.cos(0.4 + i / 2.0));
     }
 
-    private static void mathArray(double[] array) {
-        for (var i = 0; i < array.length; i++)
-            array[i] = (array[i] * Math.sin(0.2 + i / 5.0) * Math.cos(0.2 + i / 5.0) * Math.cos(0.4 + i / 2.0));
-    }
-
-    private static void mergeArray() {
-        for (var i = 0; i < array1.length; i++)
-            arr[i] = array1[i];
-        for (var i = 0; i < array2.length; i++)
-            arr[array1.length + i] = array2[i];
+    private static void mergeArray(double[] array) {
+        System.arraycopy(array, 0, arr, 0, array.length);
     }
 }
